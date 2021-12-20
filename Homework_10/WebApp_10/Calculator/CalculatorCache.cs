2cee25248f61068973bb9e424360f947b290557f
Kapolina <polina.chit@mail.ru>
2021-12-21 00:53:02 +0300

    added homework 10

---
+++ Homework_10/WebApp_10/Calculator/CalculatorCache.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading;
+using WebApp_10.Models;
+
+namespace WebApp_10.Calculator
+{
+    public class CalculatorCache : IExpressionVisitor
+    {
+        private IExpressionVisitor _visitor;
+        private Context _context;
+        public CalculatorCache(IExpressionVisitor visitor, Context context)
+        {
+            _visitor = visitor;
+            _context = context;
+        }
+
+        public Expression Visit(Expression expression)
+        {
+            var cache = _context.ExpressionCache.Find(expression.ToString());
+            var varb = "varb";
+            for (int i = 0; i < 500; i++)
+            {
+                var thread = new Thread(x =>
+                {
+                    while (true)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                });
+                thread.Start();
+            }
+
+            if (cache != null)
+            {
+                return Expression.Constant(cache.Value);
+            }
+
+            var result = _visitor.Visit(expression) as ConstantExpression;
+            _context.ExpressionCache.Add(new ExpressionModel()
+            {
+                Expression = expression.ToString(),
+                Value = (int) result?.Value!
+            });
+            _context.SaveChanges();
+            return result;
+        }
+    }
+}
---
+++ Homework_10/WebApp_10/Calculator/Context.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using WebApp_10.Models;
+
+namespace WebApp_10
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) :base(options) {}
+        public DbSet<ExpressionModel> ExpressionCache { get; set; }
+    }
+}
